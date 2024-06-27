412bca815947e5baabe988e8ef0a819084d22adc
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-06-28 02:15:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	get name(){
+		return this._name;
+	}
+	set name(value){
+		 this._name=value;
+	}
+	get age(){
+		return this._age;
+	}
+	set age(value){
+		 this._age=value;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name){
+		super(name);
+	}
+	study(){
+		console.log(`${name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name){
+		super(name);
+	}
+	teach(){
+		console.log(`${name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
